a6b066d694fa6de8c68db08482e3e21ced631de6
TaikSpace <tarcisiorodrigues454@gmail.com>
2020-09-13 16:36:53 -0300

    Avatar do usuario

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,22 @@
-import{Router} from 'express'
+//Importações
+import{Router} from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import User from './app/controllers/UserController'
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
+//Declarando as variáveis
+const upload=multer(multerConfig)
 const routes=new Router();
 //Users
 routes.post('/users',UserController.store)
 routes.put('/users',UserController.update)
+routes.use(authMiddleware);
 //Session
 routes.post('/sessions' ,authMiddleware,SessionController.store)
-
+//Upload
+routes.post('/files',upload.single('file'),(req,res)=>{
+  return res.json({ok:true});
+})
 export default routes;
